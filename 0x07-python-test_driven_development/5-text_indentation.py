62b1e9034ec8045f23f7a87e737b6d515a98a029
Markbaro <markbarasa2021@gmail.com>
2023-09-05 15:15:20 +0300

    4. Text indentation

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+This module defines `text_indentation`
+The function prints a text with 2 new lines after each of characters
+"""
+
+
+def text_indentation(text):
+    """splits a text with added two newlines after each of these characters "?", ":", "."
+    """
+
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+    
+    for delim in ".:?":
+        text = (delim + "\n\n").join(
+            [line.strip(" ") for line in text.split(delim)])
+
+    print("{}".format(text), end="")
