eee059561c79099a5ec4733a3e3eb99aed8f19dd
Markbaro <markbarasa2021@gmail.com>
2023-09-06 10:04:35 +0300

    fix word count

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -6,12 +6,12 @@ The function prints a text with 2 new lines after each of characters
 
 
 def text_indentation(text):
-    """splits a text with added two newlines after each of these characters "?", ":", "."
     """
-
+    splits a text after each of these characters "?", ":", "."
+    """
     if type(text) is not str:
         raise TypeError("text must be a string")
-    
+
     for delim in ".:?":
         text = (delim + "\n\n").join(
             [line.strip(" ") for line in text.split(delim)])
