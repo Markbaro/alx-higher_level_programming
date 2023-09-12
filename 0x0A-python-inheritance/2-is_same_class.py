762cfbae6f7e0c624ef8aa7e7fb463ea6387d1d8
Markbaro <markbarasa2021@gmail.com>
2023-09-12 13:09:51 +0300

    fix syntax error

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -9,9 +9,11 @@ def is_same_class(obj, a_class):
         obj : The object to check.
         a_class (type): The class to match the type of obj to.
     Returns:
-        True - If obj is exactly an instance of a_class - 
+        True - If obj is exactly an instance of a_class -
         Otherwise - False.
-    """
+
     if type(obj) == a_class:
         return True
     return False
+    """
+    return type(obj) is a_class
